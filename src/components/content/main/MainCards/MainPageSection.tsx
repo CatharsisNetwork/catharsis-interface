1fe7281972767d99d0f708605d63c3148ce7150f
Anton7p <https://github.com/Anton7p/Armaggeddon.git>
2021-07-26 21:35:03 +0300

    fix style main page

---
+++ src/components/content/main/MainCards/MainPageSection.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import style from "../style.module.scss";
+
+type MainPageSectionType = {
+    title: () => React.ReactNode
+    cards: () => React.ReactNode
+    footer?: () => React.ReactNode
+    custom?: string
+}
+
+function MainPageSection({title, cards, footer,custom}: MainPageSectionType) {
+    return (
+        <div className={"_container"}>
+            <div className={style.main_content}>
+                <div className={style.main_content__title}>
+                    {title()}
+                </div>
+                <div className={style.main_content__cards}>
+                    {cards()}
+                </div>
+                {footer ? (
+                    <div className={style[`main_content__${custom}`]}>
+                        {footer()}
+                    </div>
+                ) : null}
+            </div>
+        </div>
+    );
+}
+
+export default MainPageSection;
--- src/components/content/main/MainPage.tsx
+++ src/components/content/main/MainPage.tsx
@@ -1,6 +1,5 @@
 import React, {useEffect} from 'react';
 import style from "./style.module.scss";
-import main_content_1 from "../../../assets/image/main/main_content_1.png"
 import main_content_vector from "../../../assets/image/main/main_content_vector.png"
 import main_content_frame_1 from "../../../assets/image/main/main_content_frame_1.png"
 import main_content_frame_2 from "../../../assets/image/main/main_content_frame_2.png"
@@ -11,93 +10,53 @@ import CardsList from "../../ui/Card/CardsList";
 import {data} from "../../../assets/database/main/data";
 import MainCardsList from './MainCards/MainCardsList';
 import {scrollToTop} from "../../../utils/lib/scroll";
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import MainPageHeader from "./MainPageHeader";
+import MainPageSection from "./MainCards/MainPageSection";
 
 function MainPage() {
     useEffect(() => scrollToTop(), [])
     return (
         <>
             <div className={"_container"}>
-
-
                 <section className={style.main_header}>
-                    <div className={style.main_header__title}>
-                        <h1>
-                            <span>Buy, sell and mint</span><br/>
-                            <span>fractional ownership of</span> <p>NFTs</p>
-                        </h1>
-                    </div>
-                    <div className={style.main_header__description}>
-                        <span>
-                            Fractional ownership of the world’s most sought after NFTs.
-                        Fractional unlocks liquidity, reduces entry costs, and allows for
-                        synergy with other DeFi primitives.
-                        </span>
-                    </div>
-                    <div className={style.main_header__button}>
-                        <span> Live vaults</span>
-                    </div>
-                    <div className={style.main_header__image}>
-                        <img src={main_content_1} alt="main_content"/>
-                    </div>
+                    <MainPageHeader/>
                 </section>
             </div>
 
-
             <section className={style.position}>
-                <div className={"_container"}>
-                    <div className={style.main_content}>
-                        <div className={style.main_content__title}>
-                            <span> Gallery</span>
-                        </div>
-                        <div className={style.main_content__cards}>
-                            <CardsList cards={data.content.cards_content_1}/>
-                        </div>
-                        <div className={style.main_content__button}>
-                            <Link to={"gallery"}>
-                                <button> Gallery</button>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
+                <MainPageSection
+                    title={() => <span> Gallery</span>}
+                    cards={() => <CardsList cards={data.content.cards_content_1}/>}
+                    footer={() => <Link to={"gallery"}>
+                        <button> Gallery</button>
+                    </Link>}
+                    custom={"button"}
+                />
                 <img src={main_content_vector} alt="main_content_vector"/>
             </section>
 
 
             <section className={style.position}>
-                <div className={"_container"}>
-                    <div className={style.main_content}>
-                        <div className={style.main_content__title}>
-                            <span> How fractional NFTs works</span>
-                        </div>
-                        <div className={style.main_content__cards}>
-                            <MainCardsList data={data.content.main_cards}/>
-                        </div>
-                        <div className={style.main_content__action}>
-                            <img src={main_content_action} alt="main_content_action"/>
-                        </div>
-                    </div>
-                </div>
+                <MainPageSection
+                    title={() => <span> How fractional NFTs works</span>}
+                    cards={() => <MainCardsList data={data.content.main_cards}/>}
+                    footer={() => <img src={main_content_action} alt="main_content_action"/>}
+                    custom={"action"}
+                />
                 <div className={style.position_bg}/>
             </section>
 
 
             <section className={style.position}>
-                <div className={"_container"}>
-                    <div className={style.main_content}>
-                        <div className={style.main_content__title}>
-                            <span><img src={main_content_flame} alt=""/> Auction</span>
-                        </div>
-                        <div className={style.main_content__cards}>
-                            <CardsList cards={data.content.cards_content_2}/>
-                        </div>
-                        <div className={style.main_content__button}>
-                            <Link to={"gallery"}>
-                                <button> Gallery</button>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
+                <MainPageSection
+                    title={() => (<span><img src={main_content_flame} alt=""/> Auction</span>)}
+                    cards={() => <CardsList cards={data.content.cards_content_2}/>}
+                    footer={() => <Link to={"gallery"}>
+                        <button> Gallery</button>
+                    </Link>}
+                    custom={"button"}
+                />
                 <div className={style.position_frame}>
                     <img src={main_content_frame_1} alt="main_content_frame_1"/>
                 </div>
@@ -108,17 +67,10 @@ function MainPage() {
 
 
             <section className={style.position}>
-                <div className={"_container"}>
-                    <div className={style.main_content}>
-                        <div className={style.main_content__title}>
-                            <span><img src={main_content_voting} alt=""/> Voting</span>
-                            <p>Let’s choose a winner together</p>
-                        </div>
-                        <div className={style.main_content__cards}>
-                            <CardsList cards={data.content.cards_content_3}/>
-                        </div>
-                    </div>
-                </div>
+                <MainPageSection
+                    title={() => (<span><img src={main_content_voting} alt=""/> Voting</span>)}
+                    cards={() => <CardsList cards={data.content.cards_content_3}/>}
+                />
             </section>
 
         </>
