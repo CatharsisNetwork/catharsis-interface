8cb5e58615ce8aef62115b7ef1b349cb2f80b6bb
Anton7p <https://github.com/Anton7p/Armaggeddon.git>
2021-08-03 11:44:28 +0300

    fix bug

--- src/components/content/fractionalize/FractionalizeBody/LeftSide/FractionalizeCard/FractionalizeCard.tsx
+++ src/components/content/fractionalize/FractionalizeBody/LeftSide/FractionalizeCard/FractionalizeCard.tsx
@@ -1,12 +1,15 @@
 import React, {useState} from 'react';
 import style from "./style.module.scss";
 import {getActiveClassName} from "../../../../../../utils/lib/getActiveClassName";
-
+import bg from "../../../../../../assets/image/fractionalize/bg_fractionalize.png"
 
 function FractionalizeCard() {
     const [active, setActive] = useState(false)
     return (
         <div className={getActiveClassName(style, "card", active)} onClick={() => setActive(!active)}>
+            <span>
+                <img src={bg} alt="bg"/>
+            </span>
             <button className={style.card_button}>
                 <span>Sea world between fishes</span>
             </button>
